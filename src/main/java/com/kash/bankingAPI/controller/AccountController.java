6b304b1cef82489be965055929996231289f8ab2
kashifnizami@gmail.com <kashifnizami@gmail.com>
2022-01-17 16:44:45 +0100

    Primary & Savings Account

--- src/main/java/com/kash/bankingAPI/controller/AccountController.java
+++ src/main/java/com/kash/bankingAPI/controller/AccountController.java
@@ -1,19 +1,35 @@
 package com.kash.bankingAPI.controller;
 
+import com.kash.bankingAPI.entity.PrimaryAccount;
+import com.kash.bankingAPI.entity.SavingsAccount;
+import com.kash.bankingAPI.entity.User;
+import com.kash.bankingAPI.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api")
 public class AccountController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/primaryaccount")
-    public String primaryAccount(){
-        return primaryAccount();
+    public ResponseEntity<Object> primaryAccount(Principal principal){
+        User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+        return new ResponseEntity<>(primaryAccount, HttpStatus.OK);
     }
 
     @RequestMapping("/savingsaccount")
-    public String savingsAccount(){
-        return savingsAccount();
+    public ResponseEntity<Object> savingsAccount (Principal principal){
+        User user = userService.findByUsername(principal.getName());
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+        return new ResponseEntity<>(savingsAccount, HttpStatus.OK);
     }
 }
